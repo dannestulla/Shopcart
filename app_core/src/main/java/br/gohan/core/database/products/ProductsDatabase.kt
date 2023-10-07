dfeac08f15704153e44dcb756710e232577b7735
Daniel Estulla <dannestulla@gmail.com>
2023-10-07 00:52:17 -0300

    actions fix

--- app_core/src/main/java/br/gohan/core/database/products/ProductsDatabase.kt
+++ app_core/src/main/java/br/gohan/core/database/products/ProductsDatabase.kt
@@ -3,7 +3,10 @@ package br.gohan.core.database.products
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ProductsEntity::class], version = 2)
+@Database(
+    exportSchema = false,
+    entities = [ProductsEntity::class],
+    version = 2)
 abstract class ProductsDatabase : RoomDatabase() {
     abstract fun productsDao(): ProductsDao
 }
